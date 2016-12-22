9d34420a811587cf5e571af0aeffc8c0c2735d06
Jake Miller <hillsighed@gmail.com>
2016-12-22 14:05:57 -0800

    adds ability to add a new card

--- main.js
+++ main.js
@@ -1,1 +1,73 @@
 console.log('hi from js')
+
+// button event listener to add list
+jQuery('button').on('click', function() {
+  console.log('clicked')
+  addAList();
+
+});
+
+
+// function for adding a list
+var addAList = function() {
+  // create list container
+  var $listContainer = jQuery('<div>')
+  // add class
+  $listContainer.addClass('listContainer')
+  // create UList
+  var $ul = jQuery('<ul>')
+  // add class
+  $ul.addClass('list')
+  // create list Header
+  var $listHeader = jQuery('<h4>')
+  // set default list name
+  $listHeader.text('Unnamed List')
+  $listHeader.attr('contenteditable', 'true');
+  // add class
+  $listHeader.addClass('listHeader')
+  // create input box
+  var $addACard = jQuery('<input>')
+  $addACard.attr('placeholder', 'Add a card...');
+
+  $addACard.on('keypress', function(evt){
+
+    if(evt.keyCode === 13) {
+      console.log('i pressed enter')
+      console.log(jQuery(this))
+      // get the list
+      $list = jQuery(this).prev()
+      $newCardText = jQuery(this).val()
+      addACard($newCardText, $list)
+      jQuery(this).val(null)
+    }
+  })
+
+  // append elements
+  // append the list
+  $listHeader.appendTo($ul)
+  $ul.appendTo($listContainer)
+  $addACard.appendTo($listContainer)
+  // append list to body
+  jQuery('body').append($listContainer)
+};
+
+
+// function for card creation
+var addACard = function(newItem, list) {
+  // create li
+  $card = jQuery('<li>')
+  // specify class of card
+  $card.addClass('card')
+  // set attribute contentedible true
+  $card.attr('contenteditable', 'true');
+  // set card text to input val
+  $card.text(newItem)
+  // append li to ul
+  $card.appendTo(list)
+  // clear input field
+
+}
+
+// jQuery('input')
+
+// if (target.is('input')) {console.log('add a card')}
